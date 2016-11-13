9a36effa72e261189488d0fc76006537b9780aa0
ramtob <ramtob@gmail.com>
2016-11-13 21:00:31 +0200

    unit tests

--- spec/d3-parallel-links.spec.js
+++ spec/d3-parallel-links.spec.js
@@ -1,3 +1,13 @@
 var d3 = require("../build/d3-parallel-links");
 
 console.log("spec test...");
+
+describe("parallelLinksIsMethodExact", function () {
+    var result;
+
+    it ("shouls return false if method is approx", function () {
+        d3.parallelLinksSetMethodApprox();
+        result = d3.parallelLinksIsMethodExact();
+        expect(result).toBeFalsy();
+    })
+});
